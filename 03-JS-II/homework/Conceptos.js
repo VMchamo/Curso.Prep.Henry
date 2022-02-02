740e060b9a7c02b40b37af4b0523b4e8ebd16fe5
VMchamo <mchamorrosl96@gmail.com>
2022-02-02 17:28:59 -0300

    Homework JS-2 Completa

---
+++ 03-JS-II/homework/Conceptos.js
@@ -0,0 +1,4 @@
+// FOR : Es un bucle que se utiliza para realizar acciones repetitivas. 
+// &&: Es un operador logico que significa "y" o "and"
+// ||: Es un operador logico que significa "o" o "or"
+// ! : Es un operador logico que significa "no" o "not"
